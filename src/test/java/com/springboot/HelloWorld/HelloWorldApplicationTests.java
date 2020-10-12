2eb4653e835ad6eb62793fcb682e8ca920a15812
Sachin Gunjal <sachin@Sachins-MacBook-Pro.local>
2020-10-12 23:27:27 +0100

    controller and swagger update

---
+++ src/test/java/com/springboot/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
