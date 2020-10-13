ba26783c78a33b82296060e7bb0427cc3c6402e4
Sachin <0sachingunjal0@gmail.com>
2020-10-14 00:11:37 +0100

    Lambok and Logback.xml added

---
+++ src/main/java/com/springboot/HelloWorld/model/Employee.java
@@ -0,0 +1,13 @@
+package com.springboot.HelloWorld.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private int employeeNumber;
+    private String employeeName;
+    private int employeeAge;
+
+
+}
